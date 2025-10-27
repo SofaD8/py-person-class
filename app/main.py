242297137a820c68307d39fb85be83530151d2b7
Sofa <sofo4kavakula@gmail.com>
2025-10-27 20:02:41 +0200

    Fix person_list in create_person_list

--- app/main.py
+++ app/main.py
@@ -2,21 +2,22 @@ class Person:
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
+
         self.name = name
         self.age = age
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_dict in people:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_list.append(person)
+    person_list = [Person(person_dict["name"],
+                          person_dict["age"])
+                   for person_dict in people
+                   ]
 
     for i, person_dict in enumerate(people):
-        if "wife" in person_dict and person_dict["wife"] is not None:
+        if person_dict.get("wife") is not None:
             person_list[i].wife = Person.people[person_dict["wife"]]
-        elif "husband" in person_dict and person_dict["husband"] is not None:
+        elif person_dict.get("husband") is not None:
             person_list[i].husband = Person.people[person_dict["husband"]]
 
     return person_list
